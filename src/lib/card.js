557ae50462d14bc2bc33e8f92706bd9daf490de2
Mo BETTA <Mo@MBMacBookPro.local>
2016-09-26 13:50:03 -0700

    Create card types and card type array

---
+++ src/lib/card.js
@@ -0,0 +1,21 @@
+const cardClub = {
+  'name': "Club",
+  'symbol': "♣️"
+}
+
+const cardDiamond = {
+  'name': "Diamond",
+  'symbol': "♦️"
+}
+
+const cardHeart = {
+  'name': "Heart",
+  'symbol': "♥️"
+}
+
+const cardSpade = {
+  'name': "Spade",
+  'symbol': "♠️"
+}
+
+const cardTypes = [cardClub, cardDiamond, cardHeart, cardSpade]
