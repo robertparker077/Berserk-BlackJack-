e62c06126319def1c874c214bf94d4dbfc10c3c3
Mo BETTA <Compsight@aol.com>
2016-09-26 16:45:53 -0700

    Create deck with Card class and Deck class (#3)

--- src/lib/card.js
+++ src/lib/card.js
@@ -1,86 +1,45 @@
-const cardClub = {
-  name: "Club",
-  symbol: "♣️"
+const SUITS = [
+  { name: 'Club', symbol: "♣️" },
+  { name: 'Diamond', symbol: "♦️" },
+  { name: 'Heart', symbol: "♥️" },
+  { name: 'Spade', symbol: "♠️" },
+]
+
+const RANKS = [
+  { name: 'Ace', value: [ 1, 11 ] },
+  { name: 'Two', value: 2 },
+  { name: 'Three', value: 3 },
+  { name: 'Four', value: 4 },
+  { name: 'Five', value: 5 },
+  { name: 'Six', value: 6 },
+  { name: 'Seven', value: 7 },
+  { name: 'Eight', value: 8 },
+  { name: 'Nine', value: 9 },
+  { name: 'Ten', value: 10 },
+  { name: 'Jack', value: 10 },
+  { name: 'Queen', value: 10 },
+  { name: 'King', value: 10 },
+]
+
+class Card {
+  constructor( suit, rank ) {
+    this.suit = suit
+    this.rank = rank
+  }
+
+  toString() {
+    return `${this.suit.symbol} ${this.displayRank()}`
+  }
+
+  displayRank() {
+    if( this.rank.name === 'Ace' ) {
+      return 'A'
+    } else if( this.rank.value === 10 && this.rank.name !== 'Ten' ) {
+      return this.rank.name.substring( 0, 1 )
+    } else {
+      return this.rank.value
+    }
+  }
 }
 
-const cardDiamond = {
-  name: "Diamond",
-  symbol: "♦️"
-}
-
-const cardHeart = {
-  name: "Heart",
-  symbol: "♥️"
-}
-
-const cardSpade = {
-  name: "Spade",
-  symbol: "♠️"
-}
-
-const cardAce = {
-  name: "Ace",
-  value: 1,
-  value2: 11
-}
-
-const cardTwo = {
-  name: "Two",
-  value: 2
-}
-
-const cardThree = {
-  name: "Three",
-  value: 3
-}
-
-const cardFour = {
-  name: "Four",
-  value: 4
-}
-
-const cardFive = {
-  name: "Five",
-  value: 5
-}
-
-const cardSix = {
-  name: "Six",
-  value: 6
-}
-
-const cardSeven = {
-  name: "Seven",
-  value: 7
-}
-
-const cardEight = {
-  name: "Eight",
-  value: 8
-}
-
-const cardNine = {
-  name: "Nine",
-  value: 9
-}
-const cardTen = {
-  name: "Ten",
-  value: 10
-}
-const cardJack = {
-  name: "Jack",
-  value: 10
-}
-
-const cardQueen = {
-  name: "Queen",
-  value: 10
-}
-
-const cardKing = {
-  name: "King",
-  value: 10
-}
-
-const cardSuits = [cardClub, cardDiamond, cardHeart, cardSpade]
-const cardTypes = [cardAce, cardTwo, cardThree, cardFour, cardFive,cardSix, cardSeven, cardEight, cardNine, cardTen, cardJack, cardQueen, cardKing]
+export { Card as default, SUITS, RANKS }
--- src/lib/deck.js
+++ src/lib/deck.js
@@ -1,3 +1,33 @@
-const card = require('./card')
+import Card, { SUITS, RANKS } from './card'
 
-console.log(cardTypes[0])
+class Deck {
+  constructor() {
+    this.deck = this.buildDeck()
+
+    // console.log( this.deck, this.deck.length )
+    console.log( this.deck.length )
+    this.deck.forEach( card => console.log( card.toString() ))
+  }
+
+  suitReducer( memo, suit ) {
+    return memo.concat( ...RANKS.map( rank => new Card( suit, rank ) ))
+  }
+
+  buildDeck() {
+    // let cards = []
+    //
+    // for( let suitIndex = 0; suitIndex < SUITS.length; suitIndex++ ) {
+    //   for( let rankIndex = 0; rankIndex < RANKS.length; rankIndex++ ) {
+    //     cards.push( new Card( SUITS[ suitIndex ], RANKS[ rankIndex ] ))
+    //   }
+    // }
+    //
+    // return cards
+
+    return SUITS.reduce( this.suitReducer , [] )
+  }
+}
+
+export default Deck
+
+new Deck()
