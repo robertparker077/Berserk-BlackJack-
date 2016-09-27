fa1a9242b86b9a35a05109191c888f043e487b27
Mo BETTA <Mo@MBMacBookPro.local>
2016-09-27 13:56:09 -0700

    Create necessary functions to create deck and shuffle a pile of 2 decks

--- src/lib/deck.js
+++ src/lib/deck.js
@@ -1,33 +1,38 @@
-import Card, { SUITS, RANKS } from './card'
+import Card, { SUITS, TYPES } from './card'
 
 class Deck {
   constructor() {
     this.deck = this.buildDeck()
 
+    /* Different ways to test out of the created deck */
     // console.log( this.deck, this.deck.length )
-    console.log( this.deck.length )
-    this.deck.forEach( card => console.log( card.toString() ))
+    // console.log( this.deck.length )
+    // console.log(this.deck)
+    // this.deck.forEach( card => console.log( card.toString() ))
   }
 
   suitReducer( memo, suit ) {
-    return memo.concat( ...RANKS.map( rank => new Card( suit, rank ) ))
+    return memo.concat( ...TYPES.map( type => new Card( suit, type ) ))
   }
 
   buildDeck() {
+    /* Classic way to create deck */
     // let cards = []
     //
     // for( let suitIndex = 0; suitIndex < SUITS.length; suitIndex++ ) {
-    //   for( let rankIndex = 0; rankIndex < RANKS.length; rankIndex++ ) {
-    //     cards.push( new Card( SUITS[ suitIndex ], RANKS[ rankIndex ] ))
+    //   for( let typeIndex = 0; typeIndex < TYPES.length; typeIndex++ ) {
+    //     cards.push( new Card( SUITS[ suitIndex ], TYPES[ typeIndex ] ))
     //   }
     // }
-    //
     // return cards
 
+    /* ES6 Syntax compared to classic for loop above */
     return SUITS.reduce( this.suitReducer , [] )
+  }
+
+  cards() {
+    return this.deck
   }
 }
 
 export default Deck
-
-new Deck()
