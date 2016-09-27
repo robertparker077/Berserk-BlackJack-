ffa40ca6ceccbe33a764c818221a8f282f212326
Mo BETTA <Compsight@aol.com>
2016-09-27 15:45:06 -0700

    add player array (#5)

--- src/lib/userPrompt.js
+++ src/lib/userPrompt.js
@@ -1,5 +1,13 @@
+import Player from './player'
+
 const readlineSync = require('readline-sync');
 
-let playerName = readlineSync.question('What is your name?')
+let users = readlineSync.question('How many Players?: ')
+let players = []
 
-console.log(playerName)  
+for ( let index = 0; index < users; index++) {
+  let playerName = readlineSync.question('What is your name?: ')
+  let playerWallet = readlineSync.question('How much money do you want to start with?: $')
+  players.push(new Player(playerName, playerWallet))
+}
+console.log(players)
