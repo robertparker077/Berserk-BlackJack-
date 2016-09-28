eebbd700c444bf10655b9bd524081a6752f14e23
Isis <imoran@mail.ccsf.edu>
2016-09-28 09:03:10 -0700

    created game.js and game_test.js

--- src/lib/userPrompt.js
+++ src/lib/userPrompt.js
@@ -3,11 +3,14 @@ import Player from './player'
 const readlineSync = require('readline-sync');
 
 let users = readlineSync.question('How many Players?: ')
-let players = []
 
-for ( let index = 0; index < users; index++) {
-  let playerName = readlineSync.question('What is your name?: ')
-  let playerWallet = readlineSync.question('How much money do you want to start with?: $')
-  players.push(new Player(playerName, playerWallet))
+buildPlayers() {
+  const players = []
+
+  for (let index = 0; index < users; index++) {
+    let playerName = readlineSync.question('What is your name?: ')
+    let playerWallet = readlineSync.question('How much money do you want to start with?: $')
+    players.push(new Player(playerName, playerWallet))
+  }
+  return players
 }
-console.log(players)
