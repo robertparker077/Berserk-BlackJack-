bebf1c172e4c493b128009e50cf57cfc05be2aaf
Mo BETTA <Mo@MBMacBookPro.local>
2016-09-29 13:28:21 -0700

    Deal and check for blackjack

---
+++ src/test/game_test.js
@@ -0,0 +1,9 @@
+import checkForAce from './lib/hand'
+
+describe("checkForAce", () => {
+  it("Checks for a ace in a given hand of two cards", () => {
+    expect(checkForAce(Dealer.hand.[Card {suit: {name: 'Club', symbol: '♣️'},
+                                          type: {name: 'Ace', value [1,11]} })).
+    to.equal(true)
+  })
+})
